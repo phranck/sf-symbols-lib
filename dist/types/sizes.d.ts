5bab98085a01e04a29fd3a0ebfaae63fe9522ec6
Frank Gregor <phranck@mac.com>
2026-01-11 00:13:34 +0100

    Remove node_modules and dist from repository

--- dist/types/sizes.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Icon sizes mapping for SF Symbols components
- * Pixel values for different size presets
- */
-export declare const PAIconSize: Record<string, number>;
-//# sourceMappingURL=sizes.d.ts.map
