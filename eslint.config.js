679bbd83285feeb4d3b622718bb504425e0e87de
Frank Gregor <phranck@mac.com>
2026-01-11 04:17:29 +0100

    Update ESLint configuration to ignore generated files
    - Modified eslint.config.js to ignore 'docs/dist' and 'docs/scripts' directories to prevent linting warnings on generated files

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'docs/dist', 'docs/scripts'] },
   {
     extends: [
       js.configs.recommended,
